743bf9dd0e7238efb9e9ee80cddbc885ead195d7
pruno <pruno@users.noreply.github.com>
2016-07-19 15:25:58 +0100

    [Server] make Worker service execute Runners (#22)
    * Streamline runner output into duplexed Routine documents
    * Provide periodic syncs with Routine documents (provide temporary logs for long running scripts)
    * Print service logs to stdout with date

--- server/src/services/Worker.js
+++ server/src/services/Worker.js
@@ -22,15 +22,47 @@
  * @flow
  */
 
+import type {ChildProcess} from 'child_process';
 import type {Document} from 'mongoose';
+import type {LogEntry} from '../scheduler/RoutineSchema';
+import type {Interface as ReadlineInterface} from 'readline';
+import type {ReadStream} from 'fs';
 import type {OnRoutineExecutionEnd} from '../scheduler/Scheduler';
+
+type StreamBindings = Map<string, ReadlineInterface>;
 
 const AbstractService = require('./AbstractService');
 const PollingPool = require('../scheduler/PollingPool');
 const PollingThread = require('../scheduler/PollingThread');
+const Readline = require('readline');
+const {execFile} = require('child_process');
+const {Map} = require('immutable');
 
 // implement ServiceInterface
 class Worker extends AbstractService {
+
+  log(message: string): void {
+    const now = new Date().toString();
+    console.log(`[${now}] ${message}`);
+  }
+
+  bindStreams(proc: ChildProcess, callback: (entry: LogEntry) => void): StreamBindings {
+    const bindings = new Map({ 'stdout': proc.stdout, 'stderr': proc.stderr }).map(
+      (stream: ReadStream, id: string) => Readline.createInterface({ input: stream })
+    );
+
+    bindings.forEach(
+      (io: ReadlineInterface, id: string) => {
+        io.on('line', (line: string) => callback({ time: new Date(), stream: id, chunk: line }));
+      }
+    );
+
+    return bindings;
+  }
+
+  freeStreams(bindings: StreamBindings): void {
+    bindings.forEach((io: ReadlineInterface) => io.removeAllListeners());
+  }
 
   onRoutine(
     routine: Document,
@@ -39,27 +71,42 @@ class Worker extends AbstractService {
     renew: OnRoutineExecutionEnd
   ): void {
     const id = routine.get('id');
-    // FIXME actually execute a Runner
-    console.log(` > Locked routine ${id}`);
-    const time = Math.random() * (3000 - 1000) + 1000;
-    console.log(` > Executing routine ${id} as dummy for ${time} ms`);
-    setTimeout(
-      () => unlock((routine: ?Document) => { routine && console.log(` > Unlocked routine ${id}`); }),
-      time
+    const script_id = routine.get('script_id');
+
+    // FIXME no transpile by default
+    const argv = ['index.js', '--transpile', '--mode', 'runner', '--script-id', script_id];
+    const child = execFile('node', argv);
+
+    this.log(`Routine ${id} Started`);
+    routine.set('runner_start_time', new Date());
+
+    const bindings = this.bindStreams(
+      child,
+      (entry: LogEntry) => routine.get('runner_log').push(entry)
     );
+
+    const sync = (callback: (err?: Error, res?: ?Document) => void) => {
+      routine.save(callback);
+    };
+
+    const intval = this.getConfig().getInteger('sandbox.routine_sync_interval');
+    let sync_interval = setInterval(() => sync(() => this.log(`Routine ${id} Syncd`)), intval);
+
+    child.on('exit', (code) => {
+      this.freeStreams(bindings);
+      clearInterval(sync_interval);
+      routine.set('runner_end_time', new Date());
+      routine.set('runner_exit_code', code);
+      // Final sync -> complete schedule (free pool slot) -> *
+      sync((e, r) => complete(routine => this.log(`Routine ${id} Completed`)));
+    });
   }
 
   init(): void {
     const polling_interval = this.getConfig().getInteger('scheduler.preview_queue.polling_interval');
     // FIXME this arbitrarly enables only 1 thread on the preview queue -> no main queue
     const pool = new PollingPool(1);
-    const thread = PollingThread.fromPool(pool, this.getScheduler().getHighPriQueue(), polling_interval);
-
-    // FIXME remove debug logging
-    thread.on(PollingThread.events.INTERVAL, () => console.log('thread.INTERVAL'));
-    thread.on(PollingThread.events.LOCK, () => console.log('thread.pool.LOCK'));
-    thread.on(PollingThread.events.ROUTINE, () => console.log('thread.ROUTINE'));
-    thread.on(PollingThread.events.UNLOCK, () => console.log('thread.pool.UNLOCK'));
+    PollingThread.fromPool(pool, this.getScheduler().getHighPriQueue(), polling_interval);
 
     this.getScheduler().pollForRoutines(pool, this.onRoutine.bind(this));
 
