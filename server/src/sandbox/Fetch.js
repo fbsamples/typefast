72bcb1f8c2484071e79dd660222cd00c55c625e7
stringtokenizer <stringtokenizer@users.noreply.github.com>
2016-09-02 17:57:53 +0100

    Fetch (#58)
    * Initial Commit of Fetching
    
    * rename url to getURL
    
    * Only allow http(s) URLs and better error handing.
    
    * Refactor: Rename Fetch class, remove constructor, ensure fetch throws error instead of catching and put Fetch in the context within the template.
    
    * Throw errors
    
    * Refactor to follow the SyncRequest library interface/options (https://github.com/ForbesLindesay/sync-request)
    
    * remove console log and return raw body
    
    * Remove orphaned parameter in function
    
    * Swap param order to make consistent with sync-request
    
    * Code style changes

---
+++ server/src/sandbox/Fetch.js
@@ -0,0 +1,95 @@
+/**
+ * Copyright (c) 2016-present, Facebook, Inc. All rights reserved.
+ *
+ * You are hereby granted a non-exclusive, worldwide, royalty-free license to
+ * use, copy, modify, and distribute this software in source code or binary
+ * form for use in connection with the web services and APIs provided by
+ * Facebook.
+ *
+ * As with any software that integrates with the Facebook platform, your use
+ * of this software is subject to the Facebook Developer Principles and
+ * Policies [http://developers.facebook.com/policy/]. This copyright notice
+ * shall be included in all copies or substantial portions of the software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * @flow
+ */
+
+import type {RequestMethod} from '../../../sdk/src/http/Request';
+const {isURL} = require('validator');
+
+const encodeBody = require('form-urlencoded');
+const SyncRequest = require('sync-request');
+
+const MESSAGE_INVALID_URL_FORMAT = 'Invalid URL Format: Please enter a valid HTTP(S) URL';
+
+const defaultOptions = function({
+    qs = {},
+    headers = {},
+    body = '',
+    json = '',
+    followRedirects = true,
+    maxRedirects = Infinity,
+    gzip = true,
+    timeout = false,
+    socketTimeout = false,
+    retry = false,
+    retryDelay = 200,
+    maxRetries = 5,
+  } = {}): Object {
+
+  if (typeof body === 'object') {
+    body = encodeBody(body);
+  }
+
+  return {
+    qs: qs,
+    headers: headers,
+    body: body,
+    json: json,
+    followRedirects: followRedirects,
+    maxRedirects: maxRedirects,
+    gzip: gzip,
+    timeout: timeout,
+    socketTimeout: socketTimeout,
+    retry: retry,
+    retryDelay: retryDelay,
+    maxRetries: maxRetries,
+  };
+};
+
+const validPath = function(path: string): boolean {
+  return isURL(path, { protocols: ['http', 'https'] });
+};
+
+const validateUrl = function(path: string): void {
+  if (!validPath(path)) {
+    throw new Error(MESSAGE_INVALID_URL_FORMAT);
+  }
+};
+
+const fetch = function(method: RequestMethod, path: string, options: ?Object): Object {
+  options = defaultOptions(options);
+
+  validateUrl(path);
+  const out = SyncRequest(method, path, options);
+
+  return {
+    url: out.url,
+    headers: out.headers,
+    status: out.statusCode,
+    body: out.body.toString(),
+  };
+};
+
+
+module.exports = {
+  fetch: fetch
+};
